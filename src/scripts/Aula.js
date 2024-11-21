aa11c5a5af6a64830e81e64f77c2a8b3ccdd9eff
Gabriel Gutierrez <ggutierrezortegon@gmail.com>
2024-11-21 12:30:02 -0500

    Create Modals for resevation action

---
+++ src/scripts/Aula.js
@@ -0,0 +1,22 @@
+    const btnCloseModal = document.getElementById("btnCloseModal")
+    // const btnCrearReserva = document.getElementById("btnReserva")
+    const btnConfirmReserva = document.getElementById("btnConfirmReserva")
+    const btnCloseConfirmReserva = document.getElementById("btnCloseConfirmReserva")
+    const modalreserva = document.getElementById("modalReservation")
+    const modalConfirmreserva = document.getElementById("modalConfirmReservation")
+    const btnopen = document.querySelector('[data-btn-reserva]')
+    btnopen.addEventListener('click', ()=>{
+        modalreserva.showModal();
+    })
+    btnCloseModal?.addEventListener('click',()=>{
+        modalreserva.close();
+    })
+    btnCloseConfirmReserva?.addEventListener('click',()=>{
+        modalConfirmreserva.close();
+    })
+    btnConfirmReserva?.addEventListener('click',()=>{
+        modalreserva.close();
+        modalConfirmreserva.showModal();
+    })
+    console.log("funciona xd")
+   
